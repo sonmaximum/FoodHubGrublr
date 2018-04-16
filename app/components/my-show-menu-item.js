966e01d457fbdd35772b4b9258f52a96c8a93cb7
sonmaximum <maxjuchheim@gmail.com>
2018-04-16 17:34:30 -0400

    Add delete confirm
    Added a confirmation dialog before deleting item

--- app/components/my-show-menu-item.js
+++ app/components/my-show-menu-item.js
@@ -1,4 +1,10 @@
 import Component from '@ember/component'
 
 export default Component.extend({
+  actions: {
+    delete () {
+      console.log('pressed')
+      this.sendAction('delete', this.get('item'))
+    }
+  }
 })
