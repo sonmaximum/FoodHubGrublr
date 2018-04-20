09ea60ef9a68bf119f8960a09c2fef3ed20c8b1b
sonmaximum <maxjuchheim@gmail.com>
2018-04-19 21:44:05 -0400

    Use google maps embed iframe instead
    Will showed me a better way to do this that loads faster

--- app/components/location-map.js
+++ app/components/location-map.js
@@ -1,13 +1,4 @@
 import Component from '@ember/component'
-import { inject as service } from '@ember/service'
 
 export default Component.extend({
-  maps: service(),
-
-  didInsertElement () {
-    this._super(...arguments)
-    const location = this.get('location')
-    const mapElement = this.get('maps').getMapElement(location)
-    this.$('.map-container').append(mapElement)
-  }
 })
